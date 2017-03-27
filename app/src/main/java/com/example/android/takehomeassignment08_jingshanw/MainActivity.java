e22a8c9dc738a27c2ae3e0ee5b88e82db7ddbab8
jingshanW <jw3435@tc.columbia.edu>
2017-03-27 17:59:46 -0400

    finish

--- app/src/main/java/com/example/android/takehomeassignment08_jingshanw/MainActivity.java
+++ app/src/main/java/com/example/android/takehomeassignment08_jingshanw/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.android.takehomeassignment08_jingshanw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity
+{
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference titleRef = database.getReference("title");
+    private DatabaseReference personRef = database.getReference("person");
+
+    private TextView title;
+    private TextView name;
+    private TextView age;
+    private CheckBox married;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title= (TextView) findViewById(R.id.title_text_view);
+        name= (TextView) findViewById(R.id.name_text_view);
+        age= (TextView) findViewById(R.id.age_text_view);
+        married= (CheckBox) findViewById(R.id.married_checkbox);
+    }
+
+    public void setTitle(View view)
+    {
+        titleRef.setValue(title.getText().toString());
+    }
+
+    public void addPerson(View view)
+    {
+        String n=name.getText().toString();
+        int a=Integer.parseInt(age.getText().toString());
+        boolean m=((CheckBox) findViewById(R.id.married_checkbox)).isChecked();
+        personRef.push().setValue(new Person(n,a,m));
     }
 }
---
+++ app/src/main/java/com/example/android/takehomeassignment08_jingshanw/Person.java
@@ -0,0 +1,42 @@
+package com.example.android.takehomeassignment08_jingshanw;
+
+/**
+ * Created by Jing on 3/27/17.
+ */
+
+public class Person
+{
+    private String name;
+    private int age;
+    private boolean married;
+
+    public Person(String name, int age, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.married = married;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+}
